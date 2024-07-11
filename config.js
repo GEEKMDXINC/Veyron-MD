308a842b3c6d430de25d1047019b35c310cbf21c
GEEKMDXINC <herukobot67@gmail.com>
2024-07-11 18:11:18 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,31 +3,32 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 
 
 //═══════[Required Variables]════════\\
-global.owner = process.env.OWNER_NUMBER.split(",")
-global.mongodb = process.env.MONGODB_URI || "mongodb+srv://mongodbural:mongodbural@cluster0.j0d7xng.mongodb.net/?retryWrites=true&w=majority"
+global.owner = process.env.OWNER_NUMBER || '237620857930'  // Make SURE its Not Be Empty, Else Bot Stoped And Errors,
+global.mongodb = process.env.MONGODB_URI || "mongodb+srv://ahil1:787191784abhi@ahil1.kzr1tt3.mongodb.net/?retrywrites=true&w=majority"
 global.port= process.env.PORT || 5000
-global.email = 'sam@secktor.live'
-global.github = 'https://github.com/vJoker-Ofc/OREO-MD'
-global.location = 'BWP Pk'
-global.gurl = 'https://instagram.com/' // add your username
-global.sudo = process.env.SUDO || '923474187615'
-global.devs = '923474187615';
-global.website = 'https://wa.me/+923474187615' //wa.me/+25000000000000
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/ee46da705fa65f9f4ccd4.jpg'
+global.email = 'etoundiruben45@gmail.com'
+global.github = 'https://github.com/GEEKMDXINC/Veyron-MD'
+global.location = 'Lahore pakistan'
+global.gurl = 'https://instagram.com/mc_rubenx/' // add your username
+global.sudo = process.env.SUDO || "237620857930"
+global.devs = '237620857930'
+global.website = 'https://github.com/GEEKMDXINC/Veyron-MD' //wa.me/+91000000000000
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/3f3ae4aace2d012c11509.jpg'
 module.exports = {
-  botname:   process.env.BOT_NAME === undefined ? 'OREO MD' : process.env.BOT_NAME,
-  ownername: process.env.OWNER_NAME === undefined ? 'ASIF-OfC💚' : process.env.OWNER_NAME,
-  sessionName:  process.env.SESSION_ID === undefined ? 'eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoibUMzTjl3eWVhd2ErUXdpL1NBZDVHS3M5WFZPeFNxZzlxVUEvYUovcUUxdz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoib01IZnM1YnhFNUh5L0w5bmh1V1lpem5sWDhRL0FRM0lkSWRCOTJIaTcwQT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJpRzFNVEl4Z3Q4UG50Z2Y4djQ1ejArbWZwYS83MGhoLzBHMUI2QjJyTjFBPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJoQVFHT2ZZbEp3eWcxNXRtRUM5QlY4aXUya0ZMNWsxajcyKzluK0g4YVJzPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjJFUEUxcUd3UzFsZ21PTldlMTFEMGJjY29Od2RHMTkyd3FFalQrRnVRSEk9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Ilp2SFpVcWdQNEQ3TEVBWElGT1AveU50WldyS2g5TGJxVE9jRHRNaHlrQ2c9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiWU5uKzZid1RKWDNYMzVFSmtydTR0RGdrZ0gvTmcyRnRkWEFTc2E5WFJsYz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiVlNkSC8rdzhNSEdHZ3VvaWN3aWJZUmxPemR2TlBUYXhCbnE5QWtUclpXVT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Im03eC9xWThDRFA0M2dNRS94NndIQjZJY2FOdmlKQnBQWU1OU0tFdTNmSFVFd0QxQjlaL3NwT3N1c24zcTRLaVNnVmhQdDdRdWl1SkVQWmJDTlBXbER3PT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MjEsImFkdlNlY3JldEtleSI6InR6d2FMMDQvTDRVMmlYRDZhb21VOWtJaUV6cmNZbWN1NVIvdkJxU0NRajA9IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbXSwibmV4dFByZUtleUlkIjozMSwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjMxLCJhY2NvdW50U3luY0NvdW50ZXIiOjAsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJkZXZpY2VJZCI6ImhETnE1WE53UkZXajRlTy1PeTRZYUEiLCJwaG9uZUlkIjoiM2ZjNjRiYjUtYjEwZC00MmIyLWIzZTEtODI4ZWVkNTYyODY3IiwiaWRlbnRpdHlJZCI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlJ5K2FzTlZPV0pLNWJlSUZDQ2ZOeVFpeUpXMD0ifSwicmVnaXN0ZXJlZCI6ZmFsc2UsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiSWh5bWJGMm1jL2d1a2JPbXpZYUhGaklPb1ZVPSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDSW5oZy9zR0VNanF0N1FHR0JBZ0FDZ0EiLCJhY2NvdW50U2lnbmF0dXJlS2V5Ijoib3kyb0J4YTlmWnVVTjdkbjljek1MbEgvRFladk1aeEVKejREaG5McmNtWT0iLCJhY2NvdW50U2lnbmF0dXJlIjoiTFBLUGdJVWRZbXFjaUNPTG5Ic21YMDRvVjY1Q1drOFVvWHNhTUhmVjZXMjNXUGxrYWFzZUhPSGEzaGt0cWFybzFVMURlQnlXaE1hamtsVEJGZzdmRHc9PSIsImRldmljZVNpZ25hdHVyZSI6IjNFNFBoNnNqM3dZVGZYUVJsenVsazltR1dPblhLOWY2VTdIZWx5SjNReHBWV2tOYkxxQVRkTjYxbzRyTHg2YnZiR1F1TENoWFY4WGJ6bXZlOHRrVEJBPT0ifSwibWUiOnsiaWQiOiIyMzc2MjA4NTc5MzA6MzJAcy53aGF0c2FwcC5uZXQifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiMjM3NjIwODU3OTMwOjMyQHMud2hhdHNhcHAubmV0IiwiZGV2aWNlSWQiOjB9LCJpZGVudGlmaWVyS2V5Ijp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQmFNdHFBY1d2WDJibERlM1ovWE16QzVSL3cyR2J6R2NSQ2MrQTRaeTYzSm0ifX1dLCJwbGF0Zm9ybSI6ImFuZHJvaWQiLCJsYXN0QWNjb3VudFN5bmNUaW1lc3RhbXAiOjE3MjA1Nzk0MDl9' : process.env.SESSION_ID,
-  author:  process.env.PACK_INFO.split(";")[0] === undefined ? 'Asif' : process.env.PACK_INFO.split(";")[0],
+  sessionName: process.env.SESSION_ID || "",      //Put Your Session Id Here
+  author:  process.env.PACK_AUTHER ||  'VEYRON BOT',
+  packname:  process.env.PACK_NAME || 'MADE BY ALEX BUGATTI',
+
+  botname:   process.env.BOT_NAME === undefined ? "Veyron-MD" : process.env.BOT_NAME,
+  ownername: process.env.OWNER_NAME === undefined ? 'Geekbugatti' : process.env.OWNER_NAME,  
   auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
-  packname:  process.env.PACK_INFO.split(";")[1] === undefined ? 'Oreo-Md' : process.env.PACK_INFO.split(";")[1],
   autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION ,
   antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nbwoed' : process.env.ANTI_BAD_WORD,
   alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? false : process.env.ALWAYS_ONLINE,
-  antifake : process.env.FAKE_COUNTRY_CODE === undefined ? '48' : process.env.FAKE_COUNTRY_CODE,
+  antifake : process.env.FAKE_COUNTRY_CODE === undefined ? '237' : process.env.FAKE_COUNTRY_CODE,
   readmessage:  process.env.READ_MESSAGE === undefined ? false : process.env.READ_MESSAGE,
   auto_status_saver: process.env.AUTO_STATUS_SAVER === undefined ? false : process.env.AUTO_STATUS_SAVER,
-  HANDLERS:  process.env.PREFIX === undefined ? ['.'] : process.env.PREFIX,
+  HANDLERS:  process.env.PREFIX === undefined ? '.' : process.env.PREFIX,
   warncount : process.env.WARN_COUNT === undefined ? 3 : process.env.WARN_COUNT,
   disablepm:  process.env.DISABLE_PM === undefined ? false : process.env.DISABLE_PM,
   levelupmessage:  process.env.LEVEL_UP_MESSAGE === undefined ? false : process.env.LEVEL_UP_MESSAGE,
@@ -36,6 +37,7 @@ module.exports = {
   BRANCH: 'main', 
   ALIVE_MESSAGE:  process.env.ALIVE_MESSAGE === undefined ? '' : process.env.ALIVE_MESSAGE,
   autobio:  process.env.AUTO_BIO === undefined ? false : process.env.AUTO_BIO,
+  caption :process.env.CAPTION || "\t*•ᴘᴏᴡᴇʀᴇᴅ ʙʏ veyron-²²¹-ᴍᴅ•* ",   //*『sᴜʙsᴄʀɪʙᴇ • veyron ᴛᴇᴄʜ』*\n youtube.com/@blade444"),        
   OPENAI_API_KEY:  process.env.OPENAI_API_KEY === undefined ? false : process.env.OPENAI_API_KEY,
   heroku:  process.env.heroku === undefined ? false : process.env.heroku,
   HEROKU: {
@@ -44,15 +46,15 @@ module.exports = {
     APP_NAME: process.env.HEROKU_APP_NAME === undefined ? '' : process.env.HEROKU_APP_NAME
 },
   VERSION: process.env.VERSION === undefined ? 'v.0.0.3' : process.env.VERSION,
-  LANG: process.env.THEME|| 'OREO',
-  WORKTYPE: process.env.WORKTYPE === undefined ? 'private' : process.env.WORKTYPE
+  LANG: process.env.THEME|| 'GOJO',
+  WORKTYPE: process.env.WORKTYPE === undefined ? 'public' : process.env.WORKTYPE
 };
 
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
-	fs.unwatchFile(file)
-	console.log(`Update'${__filename}'`)
+        fs.unwatchFile(file)
+        console.log(`Update'${__filename}'`)
     delete require.cache[file]
-	require(file)
+        require(file)
 })
