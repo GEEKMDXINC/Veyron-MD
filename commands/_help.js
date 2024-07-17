ef13bb1f3709f4a3e98546297e378052db23aca3
GEEKMDXINC <herukobot67@gmail.com>
2024-07-17 11:34:29 +0100

    Update _help.js

--- commands/_help.js
+++ commands/_help.js
@@ -1,14 +1,16 @@
 /**
 
                                                             
- Copyright (C) 2022.
- Licensed under the  GPL-3.0 License;
- You may not use this file except in compliance with the License.
- It is supplied in the hope that it may be useful.
- * @project_name : MAC-Md
- * @author : Maccoder3 <https://github.com/Maccoder3>
- * @description : MAC,A Multi-functional whatsapp bot.
- * @version 0.0.6
+ 
+  ____      __  __   _____   _       ____      __  __   _____   _   _
+ |  _ \    |  \/  | |  __ \ | |     |  _ \    |  \/  | |  __ \ | | | |
+ | |_) |   | \  / | | |__) || |     | |_) |   | \  / | | |__) || | | |
+ |  _ <    | |\/| | |  ___/ | |     |  _ <    | |\/| | |  ___/ | | | |
+ | |_) |   | |  | | | |     | |____ | |_) |   | |  | | | |     |_| |_|
+ |____/    |_|  |_| |_|     |______||____/    |_|  |_| |_|     (_) (_)
+"""
+
+
  **/
 
 const os = require('os')
